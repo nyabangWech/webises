c64cbb10d9a9dd08fd3266a15718c77a1cb3f56a
nyabangWech <nyabangwech@gmail.com>
2024-03-25 23:01:33 +0300

    added files

--- style.js
+++ style.js
@@ -1,6 +1,14 @@
 
-
-
-
-
-
+var count = document.getElementsByClassName("count");
+        var inc = [];
+        function intervalFun() {
+            for (let i = 0; i < count.length; i++) {
+                inc.push(1);
+                if (inc[i] != count[i].getAttribute("max-data")) {
+                    inc[i]++;
+                }
+                count[i].innerHTML = inc[i];
+            }
+        }
+        setInterval(intervalFun, 200);
+        
